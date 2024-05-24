e6e908957bf2e883b85d100b07d532d581eff8f0
LuytensStar <artem05zhar@gmail.com>
2024-05-24 14:46:42 +0200

    Fixed issues with redis

--- main.py
+++ main.py
@@ -34,8 +34,7 @@ async def startup():
     """
     r = await redis.Redis(host=settings.redis_host, 
                           port=settings.redis_port, 
-                          password=settings.redis_password, 
-                          db=0,
+                          password=settings.redis_password,
                           encoding="utf-8",
                           decode_responses=True)
     await FastAPILimiter.init(r)
